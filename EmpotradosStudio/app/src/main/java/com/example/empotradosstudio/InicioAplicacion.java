8ed92c0e7b97d44357f3370e6bbb31136c54f5be
Francisco Carlos Ordóñez Córdoba <48439703+1marshmallow1@users.noreply.github.com>
2020-12-31 11:22:36 +0100

    ordenaito

--- EmpotradosStudio/app/src/main/java/com/example/empotradosstudio/InicioAplicacion.java
+++ EmpotradosStudio/app/src/main/java/com/example/empotradosstudio/InicioAplicacion.java
@@ -2,7 +2,9 @@ package com.example.empotradosstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InicioAplicacion extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class InicioAplicacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_aplicacion);
+    }
+
+    public void sendMaps(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
--- EmpotradosStudio/app/src/main/java/com/example/empotradosstudio/Login.java
+++ EmpotradosStudio/app/src/main/java/com/example/empotradosstudio/Login.java
@@ -41,7 +41,7 @@ public class Login extends AppCompatActivity {
                         if(res.moveToNext()) {
                             password = res.getString(2);
                             if(editText_password.getText().toString().equalsIgnoreCase(password)){
-                                Intent intento = new Intent(Login.this, MapsActivity.class);
+                                Intent intento = new Intent(Login.this, InicioAplicacion.class);
                                 Bundle bundle = new Bundle();
                                 bundle.putString("usuario", editText_usuario.getText().toString());
                                 intento.putExtras(bundle);
