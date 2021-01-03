05174a1c1bb522a4c946a91330eab33bec55f7cb
Francisco Carlos Ordóñez Córdoba <48439703+1marshmallow1@users.noreply.github.com>
2021-01-03 13:12:24 +0100

    Brujula

---
+++ EmpotradosStudio/app/src/main/java/com/example/empotradosstudio/Brujula.java
@@ -0,0 +1,102 @@
+package com.example.empotradosstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Brujula extends AppCompatActivity implements SensorEventListener {
+
+    private TextView brujulaTextView;
+    private ImageView brujulaImageView;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor, magnetometerSensor;
+
+    private float[] lastAccelerometer = new float[3];
+    private float[] lastMagnetometer = new float[3];
+    private float[] rotationMatrix = new float[9];
+    private float[] orientation = new float[3];
+
+    boolean isLastAccelerometerArrayCopied = false;
+    boolean isLastMagnetometerArrayCopied = false;
+
+    long lastUpdatedTime = 0;
+    float currentDegree = 0f;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brujula);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        brujulaTextView = findViewById(R.id.brujulaTextView);
+        brujulaImageView =  findViewById(R.id.brujulaImageView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor == accelerometerSensor){
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            isLastAccelerometerArrayCopied = true;
+        } else if(event.sensor == magnetometerSensor){
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            isLastMagnetometerArrayCopied = true;
+        }
+
+        if(isLastAccelerometerArrayCopied && isLastMagnetometerArrayCopied && System.currentTimeMillis() - lastUpdatedTime>250){
+            SensorManager.getRotationMatrix(rotationMatrix, null, lastAccelerometer, lastMagnetometer);
+            SensorManager.getOrientation(rotationMatrix, orientation);
+
+            float azimuthInRadians = orientation[0];
+            float azimuthInDegree = (float) Math.toDegrees(azimuthInRadians);
+
+            RotateAnimation rotateAnimation =
+                    new RotateAnimation(currentDegree, -azimuthInDegree,
+                            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            rotateAnimation.setDuration(250);
+            rotateAnimation.setFillAfter(true);
+            brujulaImageView.startAnimation(rotateAnimation);
+
+            currentDegree = -azimuthInDegree;
+            lastUpdatedTime = System.currentTimeMillis();
+
+            int x = (int) azimuthInDegree;
+            brujulaTextView.setText(x + "°");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(this, accelerometerSensor);
+        sensorManager.unregisterListener(this, magnetometerSensor);
+    }
+}
